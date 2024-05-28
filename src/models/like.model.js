2ef18e8a8e25e9c6d4871a69ce73f6171eb8f57f
Utkarsh Gupta <utkarshgupta04092003@gmail.com>
2024-05-28 10:50:50 +0530

    Created like, comment, playlist and tweet models

---
+++ src/models/like.model.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const likeSchema = new mongoose.Schema(
+    {
+        video: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Video",
+        },
+        comment: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Comment",
+        },
+        tweet: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Tweet",
+        },
+        likedBy: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+export const Like = new mongoose.model("Like", likeSchema);
---
+++ src/models/tweet.model.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const tweetSchema = new mongoose.Schema({
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    content: {
+        type: String,
+        required: true,
+    },  
+    image: {
+        type: String,
+    },
+}, {
+    timestamps: true,
+});
+
+export const Tweet = new mongoose.model("Tweet", tweetSchema);
